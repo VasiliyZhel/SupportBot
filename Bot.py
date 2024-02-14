f5b69cf0cd7c1192e78f4c3d8f3caa0323f5a413
VasiliyZhel <tars.tarson@gmail.com>
2024-02-14 17:37:28 +0300

    Создан обработчик комманды help, добавлены аннотации

--- Bot.py
+++ Bot.py
@@ -5,17 +5,30 @@ import random
 token = "6563887922:AAGSUQvRlodCLtOumSGHFoxWNAukFPDkatA"
 bot = telebot.TeleBot(token)
 
-hello = ["Hi", "Hello", "Привет", "Have a good day",]
+welcome_text = """Привет, тебя приветствует бот техподдержки!
+Чтоб оставить заявку жми /new_ticket
+Чтоб зарегистрировать нового пользователя жми /reg
+"""
+help_text = """Доступные команды:
+/start - Перезапускает бота
+/reg - Регистрирует нового пользователя
+/new_ticket - Создает новую заявку
+"""
 
 @bot.message_handler(commands=["start"])
 def start(message):
     "Обработка команды start"
-    bot.send_message(message.from_user.id, random.choice(hello))
+    bot.send_message(message.from_user.id, welcome_text)
+
+@bot.message_handler(commands=["help"])
+def start(message):
+    "Обработка команды help"
+    bot.send_message(message.from_user.id, help_text)
 
 @bot.message_handler(content_types=["text"])
 def read_text(message):
     "Функция присылает ответ на неизвестные команды"
-    bot.send_message(message.from_user.id, "Я тебя не понимаю, введи /start")
+    bot.send_message(message.from_user.id, "Я тебя не понимаю, жми /help")
 
 
 bot.polling()
